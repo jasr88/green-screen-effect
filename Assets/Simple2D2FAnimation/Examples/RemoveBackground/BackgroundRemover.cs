69555a2b44d1aa4538d39f46494bafa9f3ec1045
Alex Silva <jasr88@gmail.com>
2020-12-09 17:25:18 -0600

    # OpenCVSharp Setup And GrabCut
    - Added OpenCV plus Unity [(From Asset Store)](https://assetstore.unity.com/packages/tools/integration/opencv-plus-unity-85928)
    -Created a Scene for customize and read camera device information
    - Created a very quick, and very small, grabcut effect for realtime video 128x72 pixels

---
+++ Assets/Simple2D2FAnimation/Examples/RemoveBackground/BackgroundRemover.cs
@@ -0,0 +1,118 @@
+﻿namespace FunnyFace.Simple2D2FAnimation {
+	using UnityEngine;
+	using OpenCvSharp;
+	using System.Linq;
+	using System;
+
+	public class BackgroundRemover :CaptureCamera {
+		[Range (0, 100)]
+		public int rezisePercentage = 50;
+		[Range (1, 50)]
+		public int iterations = 5;
+
+		public int x = 1; 
+		public int y = 1; 
+		public int width = 128; 
+		public int height = 72; 
+
+		private OpenCvSharp.Rect rect;
+		private Size ns;
+			
+		public void CicleCamera() {
+			int nextCamera = DeviceIndex + 1 >= DevicesCount ? 0 : DeviceIndex + 1;
+			SetActiveCamera (nextCamera);
+		}
+
+		public void CicleFilterMode() {
+			FilterMode fm = (int)(currentDeviceInfo.filterMode + 1) > 2 ? 0 : currentDeviceInfo.filterMode + 1;
+			SetFilterMode (fm);
+		}
+
+		protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output) {
+			// 1. Obtain Frame
+			Mat img = Unity.TextureToMat (input, TextureParameters);
+
+			// 2. Rezise frame , for now the best size to analize is 10% of the original size 1280*720
+			Vector2 size = Vector2.zero;
+			size.x = img.Width * rezisePercentage / 100;
+			size.y = img.Height * rezisePercentage / 100;
+			ns = new Size (size.x, size.y);
+			Mat rzImg = img.Resize (ns);
+
+			// 3. Apply grabcut Initializating wit a center rect of Size.X-10 * Size.Y-10 and I recommend 5 iterations
+			Mat mask = new Mat (ns, MatType.CV_8U);
+			Mat bgModel = new Mat ();
+			Mat fgModel = new Mat ();
+			Cv2.GrabCut (rzImg, mask, new OpenCvSharp.Rect (1, 1, img.Width, img.Height), bgModel, fgModel, iterations, GrabCutModes.InitWithRect);
+
+			// 4. Use foreground or bg as mask
+			//mask = (mask & 1) * 255;// Takes the mask and convert every non 0 value to 255 (White) 
+
+
+
+			Mat bg = Unity.PixelsToMat(CreateBgPixels (new Color32 (0, 255, 0, 0), rzImg), rzImg.Width, rzImg.Height,false,false,0);
+			rzImg.CopyTo (bg, mask);
+			
+			// 5. Show result
+			output = Unity.MatToTexture (mask, output);
+			return true;
+		}
+
+		private Color32[] CreateBgPixels(Color32 color, Mat reference) {
+			int pixleCount = reference.Width * reference.Height * 3; // Width * Height * Depth (r,g,b)
+			Color32 colorBg = color;
+			return Enumerable.Repeat (colorBg, pixleCount).ToArray ();
+		}
+
+
+		#region Auxiliary Methods
+		private Mat ForegroundToMask(Mat foreground) {
+			//Mat mask = new Mat (foreground.Size(),MatType.CV_8U);
+			string maskVal = "";
+			for (int r = 0; r < foreground.Rows; r++) {
+				for (int c = 0; c < foreground.Cols; c++) {
+					maskVal = maskVal+" "+ foreground.Get<GrabCutClasses> (r, c);
+				/*	var pixel = foreground.Get<GrabCutClasses> (r, c);
+					if (pixel == GrabCutClasses.FGD || pixel == GrabCutClasses.PR_FGD) {
+						mask.Set (r, c, 1);
+					} else {
+						mask.Set (r, c, 0);
+					}
+				*/
+				}
+				maskVal = maskVal + "\n";
+			}
+
+			//mask = mask&1 * 255;
+			Debug.Log (maskVal);
+			
+			return foreground;
+		}
+		
+		private void convertToGrayScaleValues(Mat mask) {
+			int width = mask.Rows;
+			int height = mask.Cols;
+			Int32[] buffer = new Int32[width * height];
+			buffer = mask.Get<Int32[]> ();
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					int value = buffer[y * width + x];
+
+					if (value == Imgproc.GC_BGD) {
+						buffer[y * width + x] = 0; // for sure background
+					} else if (value == Imgproc.GC_PR_BGD) {
+						buffer[y * width + x] = 85; // probably background
+					} else if (value == Imgproc.GC_PR_FGD) {
+						buffer[y * width + x] = (byte)170; // probably foreground
+					} else {
+						buffer[y * width + x] = (byte)255; // for sure foreground
+					}
+				}
+			}
+			mask.Set<byte> (0, 0, buffer);
+		}
+		
+	}
+
+	#endregion
+}
---
+++ Assets/Simple2D2FAnimation/Scripts/CaptureCamera.cs
@@ -0,0 +1,196 @@
+﻿namespace FunnyFace.Simple2D2FAnimation {
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using OpenCvSharp;
+	using UnityEngine.UI;
+
+	public abstract class CaptureCamera :MonoBehaviour {
+
+		public delegate void DeviceReady();
+		public DeviceReady OnDeviceReady;
+
+		[SerializeField]
+		private RawImage resultImage;
+		private RectTransform parent;
+		private RectTransform imageTransform;
+		private Texture2D renderedTexture = null;
+
+		protected Unity.TextureConversionParams TextureParameters { get; private set; }
+		private Dictionary<int, DeviceAndTexture> availableDevices = new Dictionary<int, DeviceAndTexture> ();
+		private DeviceAndTexture currentDeviceAndTexture;
+		private bool canContinue = false;
+		public int DeviceIndex {
+			get {
+				return activeDeviceIndex;
+			}
+		}
+
+		public int DevicesCount {
+			get {
+				return availableDevices.Count;
+			}
+		} 
+
+		private int activeDeviceIndex = -1;
+
+		public InputDeviceData currentDeviceInfo {
+			get {
+				InputDeviceData idd = new InputDeviceData ();
+				idd.deviceName = currentDeviceAndTexture.device.name;
+				idd.resolution = "W: " + currentDeviceAndTexture.texture.width + ", H: " + currentDeviceAndTexture.texture.height;
+				idd.width = currentDeviceAndTexture.texture.width;
+				idd.height = currentDeviceAndTexture.texture.height;
+				idd.filterMode = currentDeviceAndTexture.texture.filterMode;
+				idd.isFrontCamera = currentDeviceAndTexture.device.isFrontFacing;
+				idd.isVMirror = currentDeviceAndTexture.texture.videoVerticallyMirrored;
+				idd.rotationAngle = currentDeviceAndTexture.texture.videoRotationAngle;
+				return idd;
+			}
+		}
+
+		private bool InitializeCameras() {
+			if (WebCamTexture.devices.Length <= 0) {
+				Debug.LogError ("No devices found!");
+				return false;
+			}
+
+			int index = 0;
+			foreach (WebCamDevice wcd in WebCamTexture.devices) {
+				WebCamTexture wct = new WebCamTexture (wcd.name);
+				availableDevices.Add (
+					index,
+					new DeviceAndTexture (wcd, wct)
+				);
+				index++;
+			}
+
+			SetActiveCamera (0);
+			return true;
+		}
+
+		public void SetFilterMode(FilterMode fm) {
+			currentDeviceAndTexture.texture.filterMode = fm;
+		}
+
+		public void SetActiveCamera(int index) {
+			if (activeDeviceIndex != -1) {
+				currentDeviceAndTexture.texture.Stop ();
+			}
+
+			if (availableDevices.TryGetValue (index, out currentDeviceAndTexture)) {
+				activeDeviceIndex = index;
+				currentDeviceAndTexture.texture.Play ();
+			} else {
+				Debug.LogErrorFormat ("Someting went wrong. Device {0} not found!. Setting Device Index 0", index);
+			}
+		}
+
+		private void RenderFrame() {
+			if (resultImage != null) {
+				// apply
+				resultImage.texture = renderedTexture;
+
+				// Adjust image ration according to the texture sizes 
+				//resultImage.GetComponent<RectTransform> ().sizeDelta = new Vector2 (renderedTexture.width, renderedTexture.height);
+				SizeToParent (resultImage);
+			}
+		}
+
+		private void ReadTextureConversionParameters() {
+			Unity.TextureConversionParams parameters = new Unity.TextureConversionParams ();
+			parameters.FlipHorizontally = currentDeviceAndTexture.device.isFrontFacing;
+
+			if (0 != currentDeviceAndTexture.texture.videoRotationAngle)
+				parameters.RotationAngle = currentDeviceAndTexture.texture.videoRotationAngle; // cw -> ccw
+
+			TextureParameters = parameters;
+		}
+
+		protected abstract bool ProcessTexture(WebCamTexture input, ref Texture2D output);
+
+		private IEnumerator Start() {
+			parent = resultImage.GetComponentInParent <RectTransform>();
+			imageTransform = resultImage.GetComponent<RectTransform> ();
+
+			yield return Application.RequestUserAuthorization (UserAuthorization.WebCam);
+			if (Application.HasUserAuthorization (UserAuthorization.WebCam)) {
+				if (!InitializeCameras ()) {
+					Debug.LogError ("The app can't continue");
+				}
+				canContinue = true;
+				OnDeviceReady?.Invoke ();
+			} else {
+				Debug.Log ("webcam not found");
+			}
+		}
+
+		private void Update() {
+			if (!canContinue) return;
+			if (currentDeviceAndTexture.texture != null && currentDeviceAndTexture.texture.didUpdateThisFrame) {
+				// this must be called continuously
+				ReadTextureConversionParameters ();
+
+				// process texture with whatever method sub-class might have in mind
+				if (ProcessTexture (currentDeviceAndTexture.texture, ref renderedTexture)) {
+					RenderFrame ();
+				}
+			}
+		}
+
+		public Vector2 SizeToParent(RawImage image, float padding = 0) {
+			float w = 0, h = 0;
+
+			padding = 1 - padding;
+			float ratio = image.texture.width / (float)image.texture.height;
+			var bounds = new UnityEngine.Rect (0, 0, parent.rect.width, parent.rect.height);
+			if (Mathf.RoundToInt (imageTransform.eulerAngles.z) % 180 == 90) {
+				//Invert the bounds if the image is rotated
+				bounds.size = new Vector2 (bounds.height, bounds.width);
+			}
+			//Size by height first
+			h = bounds.height * padding;
+			w = h * ratio;
+			if (w > bounds.width * padding) { //If it doesn't fit, fallback to width;
+				w = bounds.width * padding;
+				h = w / ratio;
+			}
+
+			imageTransform.SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, w);
+			imageTransform.SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, h);
+			return imageTransform.sizeDelta;
+		}
+
+
+		void OnDestroy() {
+			if (currentDeviceAndTexture != null) {
+				if (currentDeviceAndTexture.texture.isPlaying) {
+					currentDeviceAndTexture.texture.Stop ();
+					currentDeviceAndTexture.texture = null;
+				}
+			}
+		}
+	} // End of class:  Capture Camera
+
+	#region Helper Clases ==============================================================================================================================================
+	public struct InputDeviceData {
+		public string deviceName;
+		public string resolution;
+		public int width;
+		public int height;
+		public FilterMode filterMode;
+		public bool isFrontCamera;
+		public bool isVMirror;
+		public int rotationAngle;
+	}
+	public class DeviceAndTexture {
+		public WebCamDevice device;
+		public WebCamTexture texture;
+
+		public DeviceAndTexture(WebCamDevice device, WebCamTexture texture) {
+			this.device = device;
+			this.texture = texture;
+		}
+	}
+	#endregion  =========================================================================================================================================================
+}// End of namespace:  Simple2D2FAnimation
